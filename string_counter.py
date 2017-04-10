151ff06ff5c450350b39541292f08aefbf8fe86f
Pete Bachant <petebachant@gmail.com>
2017-04-09 21:18:34 -0400

    Use i rather than title setting dict key and some other fixes

--- string_counter.py
+++ string_counter.py
@@ -5,19 +5,20 @@ searchword = str(input("What string would you like to search for? "))
 results = {}
 
 #Use the filenames to set up a dictionary
-bibliography = os.listdir(filepath)
+bibliography = [f for f in os.listdir(filepath) if f.endswith("txt")]
 for title in bibliography:
     results[title]={}
 
 def countstring():
     """List the number of instances of the searchword per textfile"""
     for i in bibliography:
+        print("Attempting to find", searchword, "in", i)
         finalpath = filepath + "/" + str(i)
         try:
             with open(finalpath) as f_obj:
                 contents = f_obj.read()
-                searchcount = str(contents.lower().count(searchword))
-                results[title][searchword]=searchcount
+                searchcount = contents.lower().count(searchword)
+                results[i][searchword] = searchcount
         except FileNotFoundError:
             print("I bloody well can't find your file, m8")
     print(results)
