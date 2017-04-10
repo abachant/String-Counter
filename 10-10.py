1f1abc44cfd53f53efb2e48ef00af92f3568053f
Gbachant <Gbachant@gmail.com>
2017-04-09 20:12:12 -0400

    Update and rename 10-10.py to string_counter.py

--- 10-10.py
+++
@@ -1,7 +0,0 @@
-filepath = str(input("What is the filepath of the text you want to analyze?"))
-searchword = str(input("What do you want to search for?"))
-
-with open(filepath) as f_obj:
-    contents = f_obj.read()
-    print(contents.lower().count(searchword))
-
