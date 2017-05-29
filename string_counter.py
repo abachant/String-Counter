56d495d6cfc60a22a07f0f7bff558b2615b0514a
Gbachant <Gbachant@gmail.com>
2017-05-29 12:29:11 -0400

    Update string_counter.py
    Made it so you can record all desired strings to be searched at once

--- string_counter.py
+++ string_counter.py
@@ -1,26 +1,39 @@
 import os
 
 filepath = str(input("What directory are the files you want to analyze in? "))
-searchword = str(input("What string would you like to search for? "))
+searchwords =[]
+
+all_results = []
 
 # Get a list of text files in the chosen directory
 bibliography = [f for f in os.listdir(filepath) if f.endswith("txt")]
 
+def getstrings():
+    """Get the strings the user wants to search for"""
+    while True:
+        searchword = str(input("String to search for?('__end__' to end) "))
+        if searchword != "__end__":
+            searchwords.append(searchword)
+        else:
+            break
+
 def countstring():
-    """List the number of instances of the searchword per textfile"""
+    """List the total instances of searchword per textfiles in bibliography"""
     results = {}
+    getstrings()
     for title in bibliography:
-        print("Attempting to find", searchword, "in", title)
         results[title] = {}
         finalpath = filepath + "/" + str(title)
         try:
             with open(finalpath) as f_obj:
                 contents = f_obj.read()
-                searchcount = contents.lower().count(searchword)
-                results[title][searchword] = searchcount
+                for searchword in searchwords:
+                    searchcount = contents.lower().count(searchword)
+                    results[title][searchword] = searchcount
         except FileNotFoundError:
             print("I bloody well can't find your file, m8")
+    all_results.append(results)
     return results
 
-results = countstring()
-print(results)
+countstring()
+print(all_results)
